346dbb1247eea564e9796fb6b2ec4f9dd08ba21f
Ela Kascioglu <elakascioglu3425@gmail.com>
2021-02-08 13:50:50 +0300

    Proje dosyası ekle.

---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "- " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Sehir + " Eklendi.");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "- " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Sehir + " Silindi.");
+        }
+        public void List(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + "- " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Sehir);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0;
+            musteri1.Adi = "Ela";
+            musteri1.Soyadi = "Kaşcıoğlu";
+            musteri1.Sehir = "Istanbul";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.Adi = "Hatice";
+            musteri2.Soyadi = "Kaya";
+            musteri2.Sehir = "Malatya";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 2;
+            musteri3.Adi = "Kader";
+            musteri3.Soyadi = "Sutlu";
+            musteri3.Sehir = "Istanbul";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+            Console.WriteLine("\n---------Müşteriler Eklendi--------\n");
+
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.Delete(musteri3);
+            Console.WriteLine("\n---------Müşteriler Silindi---------\n");
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            }
+            Console.WriteLine("\n---------Müşteriler Listelendi---------\n");
+        }
+    }
+}
